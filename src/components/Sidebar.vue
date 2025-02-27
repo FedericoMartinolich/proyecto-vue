dd03a3228d96c5f9f31ad4627ca71ffbb2460a69
Federico José Martinolich <martinolichfederico@gmail.com>
2025-02-27 15:11:32 -0300

    creacion de rutas y archivos

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="sidebar">
+        <h2>Barra Lateral</h2>
+        <ul>
+            <li><router-link to="/contador">Contador</router-link></li>
+            <li><router-link to="/lista-de-tareas">Lista de Tareas</router-link></li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+    .h2{
+        color: black;
+    }
+    .sidebar{
+        width: 200px;
+        background-color: antiquewhite;
+        padding: 20px;
+    }
+</style>
